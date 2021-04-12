849e3ad44b87992851a0916f19e341da49b4e4a8
aecioprado <aecioprado@gmail.com>
2021-04-12 12:34:23 -0300

    conexao com banco postgresql OK

--- src/main/java/com/aecioprado/digitalinnovationone/jdbc/banco/Conexao.java
+++ src/main/java/com/aecioprado/digitalinnovationone/jdbc/banco/Conexao.java
@@ -1,5 +1,28 @@
 package com.aecioprado.digitalinnovationone.jdbc.banco;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
 public class Conexao {
 
+	public static void main(String[] args) {
+
+
+		Connection conexao;
+
+		String driver = "org.postgresql.Driver";
+		String url = "jdbc:postgresql://localhost:5432/digital_innovation_one_jdbc";
+		String usuario = "postgres";
+		String senha = "admin123";
+
+		try {
+			conexao = DriverManager.getConnection(url, usuario, senha);
+			System.out.println("Conexao realizada com sucesso!");
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+
+
+	}
 }
